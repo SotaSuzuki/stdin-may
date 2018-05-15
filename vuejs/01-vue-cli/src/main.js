e3443d4c5e89a145d614aa25ac3b987e97202e3b
SotaSuzuki <3.3sota.suzuki@gmail.com>
2018-05-15 17:54:11 +0900

    Add custom filter and animation (css)

--- vuejs/01-vue-cli/src/main.js
+++ vuejs/01-vue-cli/src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {
+  reverse,
+} from './customFilters'
 
 Vue.config.productionTip = false
+
+Vue.filter('reverse', reverse)
 
 new Vue({
   router,
--- vuejs/01-vue-cli/src/views/Home.vue
+++ vuejs/01-vue-cli/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <h1>{{title | reverse}}</h1>
     <h2>Users</h2>
     <ul class="user-links">
       <li><router-link :to="{ name: 'user', params: { id: 'suzuki' } }">Suzuki</router-link></li>
@@ -15,6 +15,13 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
+
+  data () {
+    return {
+      title: 'HomE',
+    }
+  },
+
   components: {
     HelloWorld,
   },
@@ -22,6 +29,39 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@keyframes rotateTitle {
+  0% {
+    color: #028;
+    transform: rotateZ(8deg)
+  }
+  25% {
+    color: #0cf;
+    transform: rotateZ(-8deg) scale(3) skewX(45deg)
+  }
+  30% {
+    color: #0cf;
+    transform: rotateZ(-8deg) scale(5) skewX(45deg)
+  }
+  50% {
+    color: #0cf;
+    transform: rotateZ(-8deg) scale(5) skewX(45deg)
+  }
+  80% {
+    color: #028;
+    transform: rotateZ(8deg)
+  }
+  100% {
+    color: #028;
+    transform: rotateZ(8deg)
+  }
+}
+h1 {
+  font-size: 50px;
+  font-family: monospace;
+  letter-spacing: .2em;
+  animation: rotateTitle ease 5s infinite;
+}
+
 .user-links {
   padding-left: 0;
   > li {
